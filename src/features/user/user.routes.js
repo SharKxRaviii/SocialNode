1470f8869c17bd7b26911fa765984ebe9eca3e84
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-07 00:35:35 +0530

    signin routes added inside user.routes.js file

--- src/features/user/user.routes.js
+++ src/features/user/user.routes.js
@@ -9,6 +9,8 @@ const userController = new UserController();
 userRoutes.post('/signup', (req, res) => {
     userController.signUp(req, res);
 });
-
+userRoutes.post('/signin', (req, res) => {
+    userController.signIn(req, res);
+});
 
 export default userRoutes;
