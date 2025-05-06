83449f5367237e9042c91ae6ff93a89343cd1b21
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-06 23:41:15 +0530

    jwtAuth added

---
+++ src/middlewares/jwt.middleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+
+export const jwtAuth = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if(!token) {
+        return res.status(401).json({message: "Unauthorized: No token provided"});
+    }
+
+    try {
+        const payload = jwt.verify(token, process.env.SECRET_KEY);
+        console.log("payload:", payload);
+        req._id = payload._id;
+        req.user = payload.user;
+
+        next();
+    } catch (error) {
+        return res.status(403).json({ message: "Invalid or expired token", error: error.message});
+    }
+}
