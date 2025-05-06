12e53bf0bdc5f0a9dcbf19cdf79f2eafe54e6d41
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-07 00:33:16 +0530

    userController signIn method added

--- src/features/user/user.controller.js
+++ src/features/user/user.controller.js
@@ -11,11 +11,42 @@ export default class UserController {
 
         try {
             const hashedPass = await bcrypt.hash(password, 12);
-            const user = await this.userRepository.signUpUser(name, email, hashedPass);
+            const newUser = await this.userRepository.signUpUser(name, email, hashedPass);
             res.status(201).json({
                 success: true,
                 msg: 'User Successfully Signed Up',
-                res: user
+                res: newUser
+            });
+        } catch (error) {
+            
+        }
+    }
+
+    async signIn(req, res) {
+        const {email, password} = req.body;
+
+        try {
+           const user = await this.userRepository.signInUser(email, password);
+           if(!user) {
+            return res.status(400).json({success: false, msg: "Incorrect Credentials!!"});
+           }
+
+           const token = jwt.sign(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            },
+            process.env.SECRET_KEY,
+            {
+                algorithm: 'HS256',
+                expiresIn: '1h'
+            }
+           );
+            res.status(200).json({
+                success: true,
+                msg: 'User Successfully Signed In',
+                res: token
             });
         } catch (error) {
             
