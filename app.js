3635b765ae28aa39df2e577b812b95a14477454e
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-08 16:57:05 +0530

    create and implement logger middleware

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import userRoutes from './src/features/user/user.routes.js';
 import { appErrorHandlerMiddleware } from './src/middlewares/appErrorHandler.middleware.js';
+import { loggerMiddleware } from './src/middlewares/logger.middleware.js';
 
 
 const app = express();
 
 app.use(express.json());
 
-
-
-
+// logger middleware
+app.use(loggerMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to PostAway-II Project');
