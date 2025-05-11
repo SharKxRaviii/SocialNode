8e868f00d4b7b6ca0b109c4e5be34f030847866d
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-11 11:19:17 +0530

    update the getUserDetailsById response and make it cleaner

--- src/features/user-profile/userProfile.controller.js
+++ src/features/user-profile/userProfile.controller.js
@@ -11,7 +11,10 @@ export default class UserProfileController {
 
         try {
             const user = await this.userProfileRepository.getUserDetailsById(userId);
-            res.status(200).json({success: true, res: user});
+            if(!user.success) {
+                return res.status(user.error.statusCode).json({success: false, msg: user.error.msg});
+            }
+            res.status(200).json({success: true, res: user.res});
         } catch (error) {
             throw new ErrorHandler("Server Error ! try again later!!",500);
         }
--- src/features/user-profile/userProfile.model.js
+++
@@ -1,7 +0,0 @@
-export default class UserProfileModel {
-    constructor(name, email, password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-} 
