37f2c3f781a27652310602b8c529a019ae6549c2
Ravi Kumar Sahoo <ravikumarsahoo.902@gmail.com>
2025-05-13 00:01:53 +0530

    togglePostLike added inside LikeRepo

--- src/features/like/like.repository.js
+++ src/features/like/like.repository.js
@@ -23,4 +23,28 @@ export default class LikeRepository {
             }
         }
     }
+
+    async togglePostLike(userId, postId) {
+        try {
+            const db = getDB();
+            const collection = db.collection(this.collectionName);
+            const existingLike = await collection.findOne({userId: new ObjectId(userId), postId: new ObjectId(postId)});
+            if(existingLike) {
+                await collection.deleteOne({_id: existingLike._id});
+                return {success: true, msg: "Post unlike successfully"};
+            }else {
+                const addLike = new LikeModel(new ObjectId(userId), new ObjectId(postId));
+                const result = await collection.insertOne(addLike);
+                return {success: true, msg: "Post liked successfully", res: result.insertedId};
+            }
+        } catch (error) {
+            return {
+                success: false,
+                error: {
+                    statusCode: 500,
+                    msg: error.message
+                }
+            }
+        }
+    }
 }
